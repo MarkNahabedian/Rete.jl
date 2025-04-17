6319cf70db7c67b80dd1fd5bbfa4436f922a3d3c
MarkNahabedian <naha@mit.edu>
2025-04-17 07:29:13 -0400

    typo.

--- src/utils.jl
+++ src/utils.jl
@@ -59,7 +59,7 @@ kb_stats(node) = kb_stats(stdout, node)
 """
     copy_facts(from_kb::AbstractReteRootNode, to_kb::AbstractReteRootNode, fact_types)
 
-Copues facts if the specified `fact_type` from `from_kb` to `to_kb`.
+Copues facts of the specified `fact_type` from `from_kb` to `to_kb`.
 
 for multiple fact types, one can broadcast over a collection of fact
 types.
